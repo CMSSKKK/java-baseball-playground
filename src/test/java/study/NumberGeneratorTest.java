160e6e67f1c65a8e806dc2bfe3a5185eb7b45151
Minseok-Choi <cms05041@gmail.com>
2021-11-22 23:56:24 +0900

    remain edit test

--- src/test/java/study/NumberGeneratorTest.java
+++ src/test/java/study/NumberGeneratorTest.java
@@ -1,10 +1,11 @@
 package study;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import ver2.NumberGenerater;
 
 import java.util.List;
@@ -28,5 +29,14 @@ public class NumberGeneratorTest {
         Assertions.assertThat(list.get(0)).isEqualTo(1);
         Assertions.assertThat(list.get(1)).isEqualTo(2);
         Assertions.assertThat(list.get(2)).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,3","1,5","2,8"})
+    @DisplayName("numberGenerator.stringToList() with ParameterizedTest")
+    void StringToListParameterizedTest(int index, int expected) {
+        List<Integer> list = numberGenerater.StringToList("358");
+
+        Assertions.assertThat(list.get(index)).isEqualTo(expected);
     }
 }
